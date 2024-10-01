d73d57ef315c4f4c5d85ee667752591505a3f3cc
imlawcs <daolehanhnguyen@gmail.com>
2024-10-01 18:12:27 +0700

    feat: setup initial project structure

--- Server-side/StreamingApp/obj/Debug/net8.0/StreamingApp.AssemblyInfo.cs
+++ Server-side/StreamingApp/obj/Debug/net8.0/StreamingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreamingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a026e1b828fa11a1d03d010e7d47d8f535c2a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f73b84b213b53610cc28323d73c8d6286a065e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreamingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreamingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
