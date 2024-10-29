d5748aad80854ae67cb177231d6b12294d877c2a
Monmoonluna <minhnguyetdn2004@gmail.com>
2024-10-29 07:07:04 +0700

    update crud user

--- Server-side/StreamingApp/obj/Debug/net8.0/StreamingApp.AssemblyInfo.cs
+++ Server-side/StreamingApp/obj/Debug/net8.0/StreamingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreamingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d5c1a69eccb84ae1952edde797825964ed0de9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d31a186020de97fec8b1733b75af4f7e5c6232")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreamingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreamingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
